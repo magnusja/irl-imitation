Use correct formula for value iteration

deep_maxent_irl.py
@@ -37,12 +37,14 @@ class DeepIRLFC:
         self.reduce_sum_sparse = tf.sparse_reduce_sum_sparse
         self.reduce_max = tf.sparse_reduce_max
         self.reduce_sum = tf.sparse_reduce_sum
+        self.sparse_transpose = tf.sparse_transpose
     else:
         self.P_a = tf.placeholder(tf.float32, shape=(n_input, n_actions, n_input))
         self.reduce_max = tf.reduce_max
         self.reduce_max_sparse = tf.reduce_max
         self.reduce_sum = tf.reduce_sum
         self.reduce_sum_sparse = tf.reduce_sum
+        self.sparse_transpose = tf.transpose
 
     self.gamma = tf.placeholder(tf.float32)
     self.epsilon = tf.placeholder(tf.float32)
@@ -83,13 +85,13 @@ class DeepIRLFC:
 
   def _vi(self, rewards):
 
-      rewards_expanded = tf.tile(tf.expand_dims(rewards, 1), [1, self.n_input])
+      rewards_expanded = rewards #tf.tile(tf.expand_dims(rewards, 1), [1, self.n_input])
 
       def body(i, c, t):
           old_values = t.read(i)
 
           expected_value = rewards_expanded + self.gamma * old_values
-          expected_value = tf.tile(tf.expand_dims(expected_value, 1), [1, tf.shape(self.P_a)[1], 1])
+          #expected_value = tf.tile(tf.expand_dims(expected_value, 1), [1, tf.shape(self.P_a)[1], 1])
 
           new_values = self.reduce_max(self.reduce_sum_sparse(self.P_a * expected_value, axis=2), axis=1)
           t = t.write(i + 1, new_values)
@@ -110,7 +112,7 @@ class DeepIRLFC:
       values = values.read(i)
 
       expected_value = rewards_expanded + self.gamma * values
-      expected_value = tf.tile(tf.expand_dims(expected_value, 1), [1, tf.shape(self.P_a)[1], 1])
+      #expected_value = tf.tile(tf.expand_dims(expected_value, 1), [1, tf.shape(self.P_a)[1], 1])
 
       if self.deterministic:
           policy = tf.argmax(self.reduce_sum(self.P_a * expected_value, axis=2), axis=1)
@@ -129,7 +131,7 @@ class DeepIRLFC:
         grid = tf.meshgrid(r, r)
         indices = tf.stack([grid[1], grid[0], tiled], axis=2)
         
-        P_a_cur_policy = tf.gather_nd(tf.transpose(self.P_a, (0, 2, 1)), indices)
+        P_a_cur_policy = tf.gather_nd(self.sparse_transpose(self.P_a, (0, 2, 1)), indices)
         P_a_cur_policy = tf.transpose(P_a_cur_policy, (1, 0))
       else:
         P_a_cur_policy = self.P_a * tf.expand_dims(policy, 2)

mdp/value_iteration.py
@@ -52,7 +52,7 @@ def value_iteration_old(P_a, rewards, gamma, error=0.01, deterministic=True):
 
     for s in range(N_STATES):
       v_s = []
-      values[s] = max([sum([P_a[s, s1, a] * (rewards[s] + gamma * values_tmp[s1]) for s1 in range(N_STATES)]) for a in
+      values[s] = max([sum([P_a[s, s1, a] * (rewards[s1] + gamma * values_tmp[s1]) for s1 in range(N_STATES)]) for a in
                        range(N_ACTIONS)])
 
     if max([abs(values[s] - values_tmp[s]) for s in range(N_STATES)]) < error:
@@ -62,7 +62,7 @@ def value_iteration_old(P_a, rewards, gamma, error=0.01, deterministic=True):
     # generate deterministic policy
     policy = np.zeros([N_STATES])
     for s in range(N_STATES):
-      policy[s] = np.argmax([sum([P_a[s, s1, a] * (rewards[s] + gamma * values[s1])
+      policy[s] = np.argmax([sum([P_a[s, s1, a] * (rewards[s1] + gamma * values[s1])
                                   for s1 in range(N_STATES)])
                              for a in range(N_ACTIONS)])
 
@@ -72,7 +72,7 @@ def value_iteration_old(P_a, rewards, gamma, error=0.01, deterministic=True):
     policy = np.zeros([N_STATES, N_ACTIONS])
     for s in range(N_STATES):
       v_s = np.array(
-        [sum([P_a[s, s1, a] * (rewards[s] + gamma * values[s1]) for s1 in range(N_STATES)]) for a in range(N_ACTIONS)])
+        [sum([P_a[s, s1, a] * (rewards[s1] + gamma * values[s1]) for s1 in range(N_STATES)]) for a in range(N_ACTIONS)])
       policy[s, :] = softmax(v_s).squeeze()
 
 
@@ -110,7 +110,7 @@ def value_iteration(P_a, rewards, gamma, error=0.01, deterministic=True):
   if chunk_size == 0:
     chunk_size = N_STATES
 
-  rewards_expanded = rewards[:, np.newaxis].repeat(N_STATES, axis=1)
+  # rewards_expanded = rewards[:, np.newaxis].repeat(N_STATES, axis=1)
   count = 0
   # estimate values
   while True:
@@ -118,9 +118,10 @@ def value_iteration(P_a, rewards, gamma, error=0.01, deterministic=True):
     values_tmp = values.copy()
 
     def step(start, end):
-      expected_value = rewards_expanded[start:end, :] + gamma * values_tmp
-      expected_value = expected_value[:, :, np.newaxis].repeat(N_ACTIONS, axis=2)
-      expected_value = np.transpose(expected_value, (0, 2, 1))
+      expected_value = rewards + gamma * values_tmp
+      #expected_value = expected_value[:, np.newaxis].repeat(N_STATES, axis=1)
+      #expected_value = expected_value[:, :, np.newaxis].repeat(N_ACTIONS, axis=2)
+      #expected_value = np.transpose(expected_value, (0, 2, 1))
       values[start:end] = (P[start:end, :, :] * expected_value).sum(axis=2).max(axis=1)
 
     with ThreadPoolExecutor(max_workers=num_cpus) as e:
@@ -139,9 +140,10 @@ def value_iteration(P_a, rewards, gamma, error=0.01, deterministic=True):
       print('VI', count)
       break
 
-  expected_value = rewards_expanded + gamma * values
-  expected_value = expected_value[:, :, np.newaxis].repeat(N_ACTIONS, axis=2)
-  expected_value = np.transpose(expected_value, (0, 2, 1))
+  expected_value = rewards + gamma * values_tmp
+  #expected_value = expected_value[:, np.newaxis].repeat(N_STATES, axis=1)
+  #expected_value = expected_value[:, :, np.newaxis].repeat(N_ACTIONS, axis=2)
+  #expected_value = np.transpose(expected_value, (0, 2, 1))
 
 
   if deterministic: